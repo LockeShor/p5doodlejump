9a68000dd3b30faff0ff5a76a94abd323aebb468
Redpug111 <75901583+Redpug111@users.noreply.github.com>
2021-10-25 14:55:45 -0400

    SEMICOLONOSCOPY

--- sketch.js
+++ sketch.js
@@ -3,17 +3,17 @@
 function setup() {
   // put setup code here
   createCanvas(500,1000)
-  var alien = createSprite(200,200)
-  alien.setImage("./doodleman.png")
+  var alien = createSprite(200,200);
+  alien.setImage("./doodleman.png");
 }
 
 function preload(){
-  loadAnimation(alien)
+  loadAnimation(alien);
 }
 
 function draw() {
   // put drawing code here
-  fill("black")
-  rect(200,200,50,50)
-  drawSprites()
+  fill("black");
+  rect(200,200,50,50);
+  drawSprites();
 }
