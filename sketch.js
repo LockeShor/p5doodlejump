8861c29edbbde7f2b57a4d7ca7faa218dc93f47a
Redpug111 <mrdoodly111@gmail.com>
2021-10-25 17:40:13 -0400

    basic image and gravity manipulation

--- sketch.js
+++ sketch.js
@@ -1,20 +1,48 @@
 
-var alien
-
-
+var alien;
+var img;
+var dx = 200, dy = 0;
+var grav = 4;
+var spaceDown = false;
+var barrier = 850;
 function setup() {
   // put setup code here
   createCanvas(500,1000)
 
-  var img = loadImage('doodleman.png')
+  img = loadImage('doodleman.png')
 
-  alien = createsprite(200,200)
-  alien.addImage(img)
+  // alien = createsprite(200,200)
+  //alien.addImage(img)
 }
 
 function draw() {
-  // put drawing code here
+  if(grav < 30){
+    grav = grav * 1.1;
+  }
+  background("white")
   fill("black");
-  rect(200,200,50,50);
-  drawSprites();
+
+  if (dy > barrier) {
+    dy -= 50;
+    grav = 4;
+  }
+  if(dy < 900){
+     dy = dy + grav;
+  }
+  else{
+    dy = 900;
+  }
+  
+
+  image(img, dx - 25,dy - 25, 100,100)
+  //drawSprites();
+  console.log("dy: " + dy)
+}
+
+function keyTyped() {
+  if (key === 'a') {
+    spaceDown = true;
+  }
+  // uncomment to prevent any default behavior
+  return false;
 }
