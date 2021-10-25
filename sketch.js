eed533cb133c6835e40674cdf3b0a334c6331790
Redpug111 <75901583+Redpug111@users.noreply.github.com>
2021-10-25 14:41:55 -0400

    sprite test

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
+
+
 function setup() {
   // put setup code here
   createCanvas(500,1000)
+  var alien = createSprite(200,200)
+  alien.setImage("./doodleman.png")
+}
+
+function preload(){
+  loadAnimation(alien)
 }
 
 function draw() {
   // put drawing code here
   fill("black")
   rect(200,200,50,50)
+  drawSprites()
 }
